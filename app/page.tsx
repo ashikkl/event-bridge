23060f0950e54ddad0c0e1b841b5676a35f87f03
Ashik K L <ashikkl19112001@gmail.com>
2023-03-09 22:35:16 +0530

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-  
+import "./page.module.css";
+import React from "react";
+
 const HomePage = () => {
   return (
-    <div className='text-3xl text-red-600 p-2'>
-      Hello Geeks!
+    <div>
+      <div>Welcome Back</div>
+      <div>
+        <label htmlFor="username">username</label>
+        <input aria-label="username" type="text" />
+        <label htmlFor="password">password</label>
+        <input aria-label="password" type="text" />
+        <input type="button" value="login" />
+        
+      </div>
     </div>
-  )
-}
-  
-export default HomePage
+  );
+};
+
+export default HomePage;
